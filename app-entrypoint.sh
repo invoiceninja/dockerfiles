d93b57181fb8185f011f6edb9e190ee3bf1465de
Sam <sam@lalop.co>
2015-12-01 16:14:11 +0100

    fix entrypoint

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -16,9 +16,9 @@ if [ ! -d /var/www/app/storage ]; then
 	mv /var/www/app/docker-backup-storage /var/www/app/storage
 else
 	IN_STORAGE_BACKUP="$(ls /var/www/app/docker-backup-storage/)"
-	for path in "$IN_STORAGE_BACKUP"; do
-		if [ ! -a "/var/www/app/storage/$path" ]; then
-			mv "/var/www/app/docker-backup-storage/$path" "/var/www/app/storage/$path"
+	for path in $IN_STORAGE_BACKUP; do
+		if [ ! -e "/var/www/app/storage/$path" ]; then
+			mv "/var/www/app/docker-backup-storage/$path" "/var/www/app/storage/"
 		fi
 	done
 fi
@@ -27,9 +27,9 @@ if [ ! -d /var/www/app/public/logo ]; then
 	mv /var/www/app/docker-backup-public-logo /var/www/app/public/logo
 else
 	IN_LOGO_BACKUP="$(ls /var/www/app/docker-backup-public-logo/)"
-	for path in "$IN_LOGO_BACKUP"; do
-		if [ ! -a "/var/www/app/public/logo/$path" ]; then
-			mv "/var/www/app/docker-backup-public-logo/$path" "/var/www/app/public/logo/$path"
+	for path in $IN_LOGO_BACKUP; do
+		if [ ! -e "/var/www/app/public/logo/$path" ]; then
+			mv "/var/www/app/docker-backup-public-logo/$path" "/var/www/app/public/logo/"
 		fi
 	done
 fi
