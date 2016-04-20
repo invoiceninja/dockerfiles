28ff00d785e9b3304f6a3310cb404517dcd7b966
Samuel Laulhau <zouliloua@gmail.com>
2016-04-20 16:15:57 +0300

    cp default shared files instead or move
    fix #5

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -13,23 +13,23 @@ echo "DB_USERNAME=$DB_USERNAME" >> .env
 echo "DB_PASSWORD=$DB_PASSWORD" >> .env
 
 if [ ! -d /var/www/app/storage ]; then
-	mv /var/www/app/docker-backup-storage /var/www/app/storage
+	cp -R /var/www/app/docker-backup-storage /var/www/app/storage
 else
 	IN_STORAGE_BACKUP="$(ls /var/www/app/docker-backup-storage/)"
 	for path in $IN_STORAGE_BACKUP; do
 		if [ ! -e "/var/www/app/storage/$path" ]; then
-			mv "/var/www/app/docker-backup-storage/$path" "/var/www/app/storage/"
+			cp -R "/var/www/app/docker-backup-storage/$path" "/var/www/app/storage/"
 		fi
 	done
 fi
 
 if [ ! -d /var/www/app/public/logo ]; then
-	mv /var/www/app/docker-backup-public-logo /var/www/app/public/logo
+	cp -R /var/www/app/docker-backup-public-logo /var/www/app/public/logo
 else
 	IN_LOGO_BACKUP="$(ls /var/www/app/docker-backup-public-logo/)"
 	for path in $IN_LOGO_BACKUP; do
 		if [ ! -e "/var/www/app/public/logo/$path" ]; then
-			mv "/var/www/app/docker-backup-public-logo/$path" "/var/www/app/public/logo/"
+			cp -R "/var/www/app/docker-backup-public-logo/$path" "/var/www/app/public/logo/"
 		fi
 	done
 fi
