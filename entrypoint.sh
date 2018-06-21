e1f8a0256b94823d68f0fcd2f18dff6ac5924373
Juan Luis Baptiste <juan.baptiste@gmail.com>
2018-06-20 21:15:02 -0500

    Create the database and user so the user does not need to create them manually.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 set -e
 
+DB_PORT=3306
+mysqlcmd="mysql -uroot -h ${DB_HOST} -P ${DB_PORT} -p${MYSQL_ROOT_PASSWORD} "
+
+function wait_for_db() {
+  while [ ! "$(mysqladmin ping -h ${DB_HOST} -P ${DB_PORT} -u root \
+              --password="${MYSQL_ROOT_PASSWORD}" --silent --connect_timeout=3)" ]; do
+    echo "Database server is not available. Waiting 2 seconds..."
+    sleep 2
+  done
+  echo "Database server is up !"
+}
+
+function create_db() {
+  echo "Creating database..."
+  $mysqlcmd -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"
+  [ $? -gt 0 ] && echo "Couldn't create database !!" && exit 23
+	echo "Creating database user and assigning permissions..."
+  $mysqlcmd -e " GRANT ALL ON ${DB_DATABASE}.* to '${DB_USERNAME}'@'%' identified by '${DB_PASSWORD}'";
+  [ $? -gt 0 ] && echo "Couldn't create database user !!" && exit 2
+	echo "Done."
+}
+
+#Wait for database to come up
+wait_for_db
+create_db
+
 if [ ! -d /var/www/app/storage ]; then
 	cp -Rp /var/www/app/docker-backup-storage /var/www/app/storage
 else
