7bf6f777ab5057e5a7af02329b964d0bfcf447df
Holger Lösken <hl@inquence.com>
2020-05-10 13:12:09 +0200

    Update to PHP config, fix indentation, fix improper user rights

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -1,26 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
 
 if [ ! -d /var/www/app/storage ]; then
-	cp -Rp /var/www/app/docker-backup-storage /var/www/app/storage
+    cp -Rp /var/www/app/docker-backup-storage /var/www/app/storage
 else
-	IN_STORAGE_BACKUP="$(ls /var/www/app/docker-backup-storage/)"
-	for path in $IN_STORAGE_BACKUP; do
-		if [ ! -e "/var/www/app/storage/$path" ]; then
-			cp -Rp "/var/www/app/docker-backup-storage/$path" "/var/www/app/storage/"
-		fi
-	done
+    IN_STORAGE_BACKUP="$(ls /var/www/app/docker-backup-storage/)"
+    for path in $IN_STORAGE_BACKUP; do
+        if [ ! -e "/var/www/app/storage/$path" ]; then
+            cp -Rp "/var/www/app/docker-backup-storage/$path" "/var/www/app/storage/"
+        fi
+    done
 fi
 
 if [ ! -d /var/www/app/public/logo ]; then
-	cp -Rp /var/www/app/docker-backup-public/logo /var/www/app/public/logo
+    cp -Rp /var/www/app/docker-backup-public/logo /var/www/app/public/logo
 else
-	IN_LOGO_BACKUP="$(ls /var/www/app/docker-backup-public/logo/)"
-	for path in $IN_LOGO_BACKUP; do
-		if [ ! -e "/var/www/app/public/logo/$path" ]; then
-			cp -Rp "/var/www/app/docker-backup-public/logo/$path" "/var/www/app/public/logo/"
-		fi
-	done
+    IN_LOGO_BACKUP="$(ls /var/www/app/docker-backup-public/logo/)"
+    for path in $IN_LOGO_BACKUP; do
+        if [ ! -e "/var/www/app/public/logo/$path" ]; then
+            cp -Rp "/var/www/app/docker-backup-public/logo/$path" "/var/www/app/public/logo/"
+        fi
+    done
 fi
 
 # compare public volume version with image version
@@ -30,18 +30,8 @@ if [ ! -e /var/www/app/public/version ] || [ "$INVOICENINJA_VERSION" != "$(cat /
   echo $INVOICENINJA_VERSION > /var/www/app/public/version
 fi
 
-# fix permission for monted directories
-chown www-data:www-data /var/www/app/storage
-chown www-data:www-data /var/www/app/public/logo
+# Set permission for mounted directories
+chown invoiceninja:www-data /var/www/app/storage
+chown invoiceninja:www-data /var/www/app/public/logo
 
-
-#php artisan optimize --force
-#php artisan migrate --force
-
-#if [ ! -e "/var/www/app/is-seeded" ]; then
-	#php artisan db:seed --force
-	#touch "/var/www/app/is-seeded"
-#fi
-
-echo 'start'
-exec "$@"
+exec docker-php-entrypoint "$@"
