997ab28eb66ff6a184b98676ec7c9b390b8a27b8
Holger Lösken <hl@inquence.com>
2020-05-20 15:28:10 +0200

    Remove checks for running entrypoint

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -7,38 +7,36 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 
-if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
-    if [ ! -d /var/www/app/storage ]; then
-        cp -Rp /var/www/app/docker-backup-storage /var/www/app/storage
-    else
-        IN_STORAGE_BACKUP="$(ls /var/www/app/docker-backup-storage/)"
-        for path in $IN_STORAGE_BACKUP; do
-            if [ ! -e "/var/www/app/storage/$path" ]; then
-                cp -Rp "/var/www/app/docker-backup-storage/$path" "/var/www/app/storage/"
-            fi
-        done
-    fi
-
-    if [ ! -d /var/www/app/public/logo ]; then
-        cp -Rp /var/www/app/docker-backup-public/logo /var/www/app/public/logo
-    else
-        IN_LOGO_BACKUP="$(ls /var/www/app/docker-backup-public/logo/)"
-        for path in $IN_LOGO_BACKUP; do
-            if [ ! -e "/var/www/app/public/logo/$path" ]; then
-                cp -Rp "/var/www/app/docker-backup-public/logo/$path" "/var/www/app/public/logo/"
-            fi
-        done
-    fi
-
-    # compare public volume version with image version
-    if [ ! -e /var/www/app/public/version ] || [ "$INVOICENINJA_VERSION" != "$(cat /var/www/app/public/version)" ]; then
-        cp -au /var/www/app/docker-backup-public/* /var/www/app/public/
-        echo $INVOICENINJA_VERSION > /var/www/app/public/version
-    fi
-
-    # Set permission for mounted directories
-    chown invoiceninja:www-data /var/www/app/storage
-    chown invoiceninja:www-data /var/www/app/public
+if [ ! -d /var/www/app/storage ]; then
+    cp -Rp /var/www/app/docker-backup-storage /var/www/app/storage
+else
+    IN_STORAGE_BACKUP="$(ls /var/www/app/docker-backup-storage/)"
+    for path in $IN_STORAGE_BACKUP; do
+        if [ ! -e "/var/www/app/storage/$path" ]; then
+            cp -Rp "/var/www/app/docker-backup-storage/$path" "/var/www/app/storage/"
+        fi
+    done
 fi
+
+if [ ! -d /var/www/app/public/logo ]; then
+    cp -Rp /var/www/app/docker-backup-public/logo /var/www/app/public/logo
+else
+    IN_LOGO_BACKUP="$(ls /var/www/app/docker-backup-public/logo/)"
+    for path in $IN_LOGO_BACKUP; do
+        if [ ! -e "/var/www/app/public/logo/$path" ]; then
+            cp -Rp "/var/www/app/docker-backup-public/logo/$path" "/var/www/app/public/logo/"
+        fi
+    done
+fi
+
+# compare public volume version with image version
+if [ ! -e /var/www/app/public/version ] || [ "$INVOICENINJA_VERSION" != "$(cat /var/www/app/public/version)" ]; then
+    cp -au /var/www/app/docker-backup-public/* /var/www/app/public/
+    echo $INVOICENINJA_VERSION > /var/www/app/public/version
+fi
+
+# Set permission for mounted directories
+chown invoiceninja:www-data /var/www/app/storage
+chown invoiceninja:www-data /var/www/app/public
 
 exec docker-php-entrypoint "$@"
