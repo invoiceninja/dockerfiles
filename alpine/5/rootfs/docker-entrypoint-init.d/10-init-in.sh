60d544cb01cf4346bc09b029b66d9fb659f1d538
Ffaen <iam@ffaen.co.uk>
2021-12-25 22:57:34 +0000

    Escape email and password upon IN User Creation
    Fixes #415

--- alpine/5/rootfs/docker-entrypoint-init.d/10-init-in.sh
+++ alpine/5/rootfs/docker-entrypoint-init.d/10-init-in.sh
@@ -3,12 +3,13 @@
 php artisan db:seed --force
 
 # Build up array of arguments...
+# We escape the env variables in order to allow special characters
 if [[ ! -z "${IN_USER_EMAIL}" ]]; then
-    email="--email ${IN_USER_EMAIL}"
+    email="--email \"${IN_USER_EMAIL}\""
 fi
 
 if [[ ! -z "${IN_PASSWORD}" ]]; then
-    password="--password ${IN_PASSWORD}"
+    password="--password \"${IN_PASSWORD}\""
 fi
 
 php artisan ninja:create-account $email $password
