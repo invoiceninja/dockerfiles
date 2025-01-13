cb4120fc54d6d255bcb74754df6705aee3efda15
Benjamin Brummer <info@benjamin-brummer.de>
2025-01-13 16:31:39 +0000

    Multistage, single init.sh

--- debian/scripts/init.sh
+++ debian/scripts/init.sh
@@ -1,33 +1,4 @@
-#!/bin/sh
-set -e
-
-in_log() {
-    local type="$1"
-    shift
-    printf '%s [%s] [Entrypoint]: %s\n' "$(date -u '+%Y-%m-%dT%H:%M:%SZ')" "$type" "$*"
-}
-
-docker_process_init_files() {
-    echo
-    local f
-    for f; do
-        case "$f" in
-        *.sh)
-            # https://github.com/docker-library/postgres/issues/450#issuecomment-393167936
-            # https://github.com/docker-library/postgres/pull/452
-            if [ -x "$f" ]; then
-                in_log INFO "$0: running $f"
-                "$f"
-            else
-                in_log INFO "$0: sourcing $f"
-                . "$f"
-            fi
-            ;;
-        *) in_log INFO "$0: ignoring $f" ;;
-        esac
-        echo
-    done
-}
+#!/bin/sh -eu
 
 if [ "$*" = 'supervisord -c /etc/supervisor/conf.d/supervisord.conf' ]; then
     # Workaround for application updates
@@ -61,18 +32,20 @@ if [ "$*" = 'supervisord -c /etc/supervisor/conf.d/supervisord.conf' ]; then
         runuser -u www-data -- php artisan migrate --force
 
         # If first IN run, it needs to be initialized
-        echo "Checking initialization status..."
-        IN_INIT=$(php -d opcache.preload='' artisan tinker --execute='echo Schema::hasTable("accounts") && !App\Models\Account::all()->first();')
-        echo "IN_INIT value: $IN_INIT"
+        if [ "$(php -d opcache.preload='' artisan tinker --execute='echo Schema::hasTable("accounts") && !App\Models\Account::all()->first();')" = "1" ]; then
+            echo "Running initialization..."
 
-        if [ "$IN_INIT" = "1" ]; then
-            echo "Running initialization scripts..."
-            docker_process_init_files /docker-entrypoint-init.d/*
+            php artisan db:seed --force
+
+            if [ -n "${IN_USER_EMAIL}" ] && [ -n "${IN_PASSWORD}" ]; then
+                php artisan ninja:create-account --email "${IN_USER_EMAIL}" --password "${IN_PASSWORD}"
+            else
+                echo "Initialization failed - Set IN_USER_EMAIL and IN_PASSWORD in .env"
+                exit 1
+            fi
         fi
 
         echo "Production setup completed"
-        echo "IN_INIT value: $IN_INIT"
-
     fi
 
     echo "Starting supervisord..."
